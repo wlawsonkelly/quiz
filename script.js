71ca3c631bf5ab725cac3274d34f94657b189e28
Lawson Kelly <wlawsonkelly@gmail.com>
2020-11-04 18:04:43 -0800

    Unorthadox but accurate. Completed Q 1

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ var quizRowEl = document.querySelector("#quiz-row");
 var quizListEl = document.querySelector("#quiz-list");
 var timeSpanEl = document.querySelector("#time-remaining");
 
-var questionArray = [{title: "Which state is NYC in", choices: ["NY", "CA", "NH"], answer: "NY"}, {title: "What color is the ocean", choices: ["blue", "red", "green"], answer: "blue"}];
-var seconds = 0;
+var questionArray = [{title: "Which state is NYC in", choices: ["NY", "CA", "NH"], answer: "NY"}, {title: "What color is the ocean", choices: ["blue", "red", "green"], answer: "blue"}, {title: "How many time zones are in Russia?", choices: ["11", "6", "9"], answer: "11"}];
+var seconds = 50;
+
+var numberCorrect = 0;
+
 startButtonEl.addEventListener("click", function(event) {
     event.target.setAttribute("style", "visibility: hidden;")
     console.log("clicked");
@@ -23,7 +26,20 @@ startButtonEl.addEventListener("click", function(event) {
 
     startTimer();
     // If statement to match choice index selected . text with answer
+});
 
+quizListEl.addEventListener("click", function(event){
+    if (event.target.matches("button")) {
+        console.log(event.target.id);
+        //I should use 0 <= 2
+        if (event.target.id === "0" || event.target.id === "1" || event.target.id === "2" ) {
+            console.log("q 1");
+            if (event.target.id === "0") {
+                numberCorrect++;
+                console.log(numberCorrect);
+            }
+        }
+    }
 });
 
 function startTimer() {
@@ -35,7 +51,6 @@ function startTimer() {
         clearInterval(timerInterval);
         goToScorePage();
       }
-  
     }, 1000);
   }
 
