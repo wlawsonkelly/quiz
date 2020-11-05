ebb32130383f63b374d231b45c5044b98ea8ffd1
Lawson Kelly <wlawsonkelly@gmail.com>
2020-11-04 18:25:46 -0800

    Quiz part done. Boy it looks ugly

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ var quizRowEl = document.querySelector("#quiz-row");
 var quizListEl = document.querySelector("#quiz-list");
 var timeSpanEl = document.querySelector("#time-remaining");
 
-var questionArray = [{title: "Which state is NYC in", choices: ["NY", "CA", "NH"], answer: "NY"}, {title: "What color is the ocean", choices: ["blue", "red", "green"], answer: "blue"}, {title: "How many time zones are in Russia?", choices: ["11", "6", "9"], answer: "11"}];
+var questionArray = [{title: "Which state is NYC in", choices: ["NY", "CA", "NH"], answer: "NY"}, {title: "What color is the ocean", choices: ["green", "red", "blue"], answer: "blue"}, {title: "How many time zones are in Russia?", choices: ["11", "6", "9"], answer: "11"}];
 var seconds = 50;
 
 var numberCorrect = 0;
+
+var currentQuestion = 1;
 
 startButtonEl.addEventListener("click", function(event) {
     event.target.setAttribute("style", "visibility: hidden;")
@@ -17,7 +19,6 @@ startButtonEl.addEventListener("click", function(event) {
     for (var i = 0; i < questionArray[0].choices.length; i++) {
      var choiceEl = document.createElement("li");
      var buttonEl = document.createElement("button");
-     buttonEl.setAttribute("style", "background-color: blue;");
      buttonEl.setAttribute("id", i);
      buttonEl.textContent = questionArray[0].choices[i];
      choiceEl.append(buttonEl);
@@ -32,12 +33,27 @@ quizListEl.addEventListener("click", function(event){
     if (event.target.matches("button")) {
         console.log(event.target.id);
         //I should use 0 <= 2
-        if (event.target.id === "0" || event.target.id === "1" || event.target.id === "2" ) {
+        if (currentQuestion === 1) {
             console.log("q 1");
             if (event.target.id === "0") {
                 numberCorrect++;
                 console.log(numberCorrect);
-            }
+                goToQestion2();
+            } 
+        } else if (currentQuestion === 2) {
+            console.log("q 2");
+            if (event.target.id === "2") {
+                numberCorrect++;
+                goToQestion3();
+                console.log(numberCorrect);
+            } 
+        } else if (currentQuestion === 3) {
+            console.log("q 3");
+            if (event.target.id === "0") {
+                numberCorrect++;
+                console.log(numberCorrect);
+                goToScorePage();
+            } 
         }
     }
 });
@@ -53,6 +69,41 @@ function startTimer() {
       }
     }, 1000);
   }
+
+function goToQestion2() {
+    quizListEl.innerHTML = "";
+    currentQuestion++;
+    seconds = seconds - 10;
+    var titleLiEl = document.createElement("li").textContent = questionArray[1].title
+    quizListEl.append(titleLiEl);
+    for (var i = 0; i < questionArray[1].choices.length; i++) {
+        console.log("starting loop");
+        var choiceEl = document.createElement("li");
+        var buttonEl = document.createElement("button");
+        buttonEl.setAttribute("id", i);
+        buttonEl.textContent = questionArray[1].choices[i];
+        choiceEl.append(buttonEl);
+        quizListEl.append(choiceEl);
+       }
+}
+
+function goToQestion3() {
+    quizListEl.innerHTML = "";
+    currentQuestion++;
+    seconds = seconds - 10;
+    var titleLiEl = document.createElement("li").textContent = questionArray[2].title
+    quizListEl.append(titleLiEl);
+    for (var i = 0; i < questionArray[2].choices.length; i++) {
+        console.log("starting loop");
+        var choiceEl = document.createElement("li");
+        var buttonEl = document.createElement("button");
+        buttonEl.setAttribute("id", i);
+        buttonEl.textContent = questionArray[2].choices[i];
+        choiceEl.append(buttonEl);
+        quizListEl.append(choiceEl);
+       }
+}
+
 
 function goToScorePage() {
     alert("Here is your score");
