2b8f1e6b7e946d8801fb570df5440bb7e9ad8d7e
Lawson Kelly <wlawsonkelly@gmail.com>
2020-11-04 19:39:42 -0800

    added comments to guide grader

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// vars from html 
+
 var startButtonEl = document.querySelector("#start-button");
 var quizRowEl = document.querySelector("#quiz-row");
 var quizListEl = document.querySelector("#quiz-list");
@@ -5,6 +7,8 @@ var timeSpanEl = document.querySelector("#time-remaining");
 var activeDiv = document.querySelector("#active-div");
 
 var highScoreEl = document.querySelector("#high-score");
+
+//js only vars
 
 var questionArray = [{title: "Which state is NYC in", choices: ["NY", "CA", "NH"]}, {title: "What color is the ocean", choices: ["green", "red", "blue"]}, {title: "How many time zones are in Russia?", choices: ["11", "6", "9"]}];
 var seconds = 50;
@@ -15,23 +19,28 @@ var currentQuestion = 1;
 
 var yourInitials = "";
 // empty timer var
+
 var timerFunc;
 
 gethighScore();
 
-
+//get high score and display in that span
 function gethighScore() {
     var highScore = localStorage.getItem("score");
     highScoreEl.textContent = highScore;
 }
 
+//Start button starts the quiz on click
 startButtonEl.addEventListener("click", function(event) {
-    
+    //hide the start button
     event.target.setAttribute("style", "visibility: hidden;");
     console.log("clicked");
+    //Show the quiz list
     quizRowEl.setAttribute("style", "visibility: visible;");
+    //set title to q1
     var titleLiEl = document.createElement("li").textContent = questionArray[0].title
     quizListEl.append(titleLiEl);
+    //loop through q1 to get lis for the quizList ul
     for (var i = 0; i < questionArray[0].choices.length; i++) {
      var choiceEl = document.createElement("li");
      var buttonEl = document.createElement("button");
@@ -40,17 +49,19 @@ startButtonEl.addEventListener("click", function(event) {
      choiceEl.append(buttonEl);
      quizListEl.append(choiceEl);
     }
+    //set and start timer
     seconds = 50;
     timeSpanEl.setAttribute("style", "visibility: visible;");
     startTimer();
 });
 
+//Logic for figureing out if answer is correct
 quizListEl.addEventListener("click", function(event){
     if (event.target.matches("button")) {
         console.log(event.target.id);
-        //I should use 0 <= 2
+        
         if (currentQuestion === 1) {
-            console.log("q 1");
+            console.log("q1");
             if (event.target.id === "0") {
                 numberCorrect++;
                 console.log(numberCorrect);
@@ -60,7 +71,7 @@ quizListEl.addEventListener("click", function(event){
                 goToQestion2();
             }
         } else if (currentQuestion === 2) {
-            console.log("q 2");
+            console.log("q2");
             if (event.target.id === "2") {
                 numberCorrect++;
                 goToQestion3();
@@ -70,7 +81,7 @@ quizListEl.addEventListener("click", function(event){
                 goToQestion3();
             }
         } else if (currentQuestion === 3) {
-            console.log("q 3");
+            console.log("q3");
             if (event.target.id === "0") {
                 numberCorrect++;
                 console.log(numberCorrect);
@@ -83,7 +94,7 @@ quizListEl.addEventListener("click", function(event){
         }
     }
 });
-
+//Found the below code via stack overflow https://stackoverflow.com/questions/49425137/how-to-stop-timer-in-javascript
 function startTimer() {
 timerFunc = setInterval(function() {
      seconds--;
@@ -98,14 +109,16 @@ timerFunc = setInterval(function() {
 function stopTimer() {
     clearInterval(timerFunc);
 }
-//Found the above code via stack overflow
 
 function goToQestion2() {
+    //clear the quizlist
     quizListEl.innerHTML = "";
+    //go to the next question so if else in the quizlist event listener works
     currentQuestion++;
-    
+    //set title etc
     var titleLiEl = document.createElement("li").textContent = questionArray[1].title
     quizListEl.append(titleLiEl);
+    //for loop like start button event listener
     for (var i = 0; i < questionArray[1].choices.length; i++) {
         console.log("starting loop");
         var choiceEl = document.createElement("li");
@@ -116,11 +129,10 @@ function goToQestion2() {
         quizListEl.append(choiceEl);
        }
 }
-
+//same as goToQestion2
 function goToQestion3() {
     quizListEl.innerHTML = "";
     currentQuestion++;
-    seconds = seconds - 10;
     var titleLiEl = document.createElement("li").textContent = questionArray[2].title
     quizListEl.append(titleLiEl);
     for (var i = 0; i < questionArray[2].choices.length; i++) {
@@ -136,9 +148,11 @@ function goToQestion3() {
 
 
 function goToScorePage() {
+    //give user their score
     alert("Here is your score: " + numberCorrect);
+    //wipe the quiz list
     quizListEl.innerHTML = "";
-
+    //create submit button and inital input and add them to the center div
     var initialInput = document.createElement("input");
     initialInput.setAttribute("type", "text");
     activeDiv.append(initialInput);
@@ -160,14 +174,14 @@ function submitScore(initials) {
     var highestScore = parseInt(localStorage.getItem("score"));
 
     if (highestScore > numberCorrect) {
+        //checking if we have a high score
         alert("Your score was lower than your high score");
-        startButtonEl.setAttribute("style", "visibility: visible;");
-        quizRowEl.setAttribute("style", "visibility: hidden;");
-        
     } else {
-        alert("Your score has been saved");
+        // WE HAVE A HIGH SCORE and need to save it
+        alert("Your high score has been saved");
         localStorage.setItem("score", numberCorrect);
     }
+    //Reset score and reset the quiz elements to start
     numberCorrect = 0;
     startButtonEl.setAttribute("style", "visibility: visible;");
     quizRowEl.setAttribute("style", "visibility: hidden;");
@@ -177,3 +191,4 @@ function submitScore(initials) {
     activeDiv.removeChild(activeDiv.lastChild);
 }
 
+//Thanks for reading 
